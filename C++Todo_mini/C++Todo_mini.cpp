7f203cdd1f8335ee74deb2204d2b541011bd5b34
take0708 <pk_er_stbo@yahoo.co.jp>
2024-10-14 15:00:37 +0900

    Initial commit with consistent line endings

---
+++ C++Todo_mini/C++Todo_mini.cpp
@@ -0,0 +1,193 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "C++Todo_mini.h"
+
+#include <vector>
+#include <string>
+
+#define MAX_LOADSTRING 100
+
+// グローバル変数:
+HINSTANCE hInst;                                // 現在のインターフェイス
+WCHAR szTitle[MAX_LOADSTRING];                  // タイトル バーのテキスト
+WCHAR szWindowClass[MAX_LOADSTRING];            // メイン ウィンドウ クラス名
+
+HWND hEdit, hButton, hListBox; // タスク入力用のテキストボックス、追加ボタン、リストボックス
+std::vector<std::wstring> taskList; // タスクのリスト
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // グローバル文字列を初期化する
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_CTODOMINI, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // アプリケーション初期化の実行:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CTODOMINI));
+
+    MSG msg;
+
+    // メイン メッセージ ループ:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+//  関数: MyRegisterClass()
+//  目的: ウィンドウ クラスを登録します。
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CTODOMINI));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_CTODOMINI);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//   関数: InitInstance(HINSTANCE, int)
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // グローバル変数にインスタンス ハンドルを格納する
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//  目的: メイン ウィンドウのメッセージを処理します。
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_CREATE:
+        // テキストボックスを作成
+        hEdit = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER,
+            50, 50, 200, 20, hWnd, NULL, hInst, NULL);
+
+        // ボタンを作成
+        hButton = CreateWindowW(L"BUTTON", L"Add Task", WS_CHILD | WS_VISIBLE,
+            50, 80, 100, 30, hWnd, (HMENU)1, hInst, NULL);
+
+        // リストボックスを作成
+        hListBox = CreateWindowW(L"LISTBOX", NULL, WS_CHILD | WS_VISIBLE | LBS_STANDARD,
+            50, 120, 200, 200, hWnd, (HMENU)2, hInst, NULL);
+        break;
+
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        if (wmId == 1) // 「タスクを追加」ボタンの処理
+        {
+            wchar_t taskText[256];
+            GetWindowTextW(hEdit, taskText, 256);
+
+            // タスクをリストに追加
+            taskList.push_back(taskText);
+
+            // リストボックスにタスクを追加
+            SendMessageW(hListBox, LB_ADDSTRING, 0, (LPARAM)taskText);
+
+            // テキストボックスをクリア
+            SetWindowTextW(hEdit, L"");
+        }
+        else if (wmId == 2 && HIWORD(wParam) == LBN_DBLCLK) // リストボックスのダブルクリックでタスクを削除
+        {
+            int index = (int)SendMessageW(hListBox, LB_GETCURSEL, 0, 0);
+            if (index != LB_ERR)
+            {
+                // リストからタスクを削除
+                taskList.erase(taskList.begin() + index);
+                SendMessageW(hListBox, LB_DELETESTRING, (WPARAM)index, 0);
+            }
+        }
+        else
+        {
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
